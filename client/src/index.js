5e13b54d2bc9d3422b1492b2359e84116db07e56
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-08-01 15:11:48 -0300

    Criando o projeto do CRUD usando NODE.js como BACKEND

---
+++ client/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+
+/**
+ *                       FRONT_END
+ *                   ____     ______________            *                 
+ *     *            /___/|   /_____________/|                             
+ *                  |   ||   |    _________|/    *                     
+ *       *          |   ||   |    | |______                            
+ *   *              |   ||   |    |/______/|                                          
+ *       *   ___    |   ||   |________    ||         *            
+ *          /  /|___|   ||    ________|   ||
+ *    *    |  |_/___|   ||   /________|   ||
+ *          \__________//    |____________|/    *
+ * 
+ * 
+ *  + Developed by : GabrielDantasOli
+ *  + GITHUB       : https://github.com/gabrieldantasoli
+ *  + Contact      : gabriel.dantas.oliveira@ccc.ufcg.edu.br
+ *  + JAVASCRIPT FOR ALL LIFE!
+ * 
+ **/ 
---
+++ client/src/routes/AppRoutes.js
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from '../pages';
+
+
+export default () => {
+    
+    return(
+        <Routes>
+            <Route path='/' element={<Home />} />
+        </Routes>
+    );
+};
